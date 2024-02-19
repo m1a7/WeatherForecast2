e230f7cb520a0a1a310778ad2e4f671a41cebe2c
m1a7 <without@mail.com>
2024-02-19 14:38:36 +0100

    Initial Commit

---
+++ WeatherForecast2/WeatherForecast2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecast2App.swift
+//  WeatherForecast2
+//
+//  Created by Admin on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecast2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
